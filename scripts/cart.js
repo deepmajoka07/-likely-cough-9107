897218106221d1b28fbac99131aa641c45b3a0f7
MOHITNEGI16 <mohitibps2023@gmail.com>
2023-01-22 22:34:05 +0530

    order and checkout

---
+++ scripts/cart.js
@@ -0,0 +1,163 @@
+let cartData= JSON.parse(localStorage.getItem('cartData')) || []
+
+
+let container = document.getElementById('container')
+let quantity = document.getElementById('quantity')
+let subTotal = document.getElementById('subtotalforsummry')
+let totalEst = document.getElementById('totalestimate')
+let guestcheckout = document.getElementById('guestcheckout');
+
+guestcheckout.addEventListener("click",()=>{
+    if(cartData.length){
+        window.location.href = "./checkout.html"
+    }
+    alert("Cart is Empty")
+})
+
+// giving functionallity part
+
+RenderData(cartData);
+
+
+function valueAppend(data){
+    let total = 0;
+
+    data.forEach((el) => {
+        if(el.count !== undefined){
+            total+= +el.price.slice(1)*Number(el.count);
+        }else{
+            total+= +el.price.slice(1);
+        }
+    });
+    total = total.toFixed(1)
+    subTotal.innerText = "$"+total;
+    totalEst.innerText = "$"+(+total+ 45);
+
+}
+
+function RenderData(cartData) {
+    let htmlData = cartData.map((element,ind) => {
+        return `
+        <div class="main_container">
+    
+        <div id="image" style="height: 99%;width: 95%;">
+            <img src=${element.image_1}
+                alt="" style="height: 100%;width: 100%;">
+        </div>
+    
+        <div id="information" >
+            <h3 id="title">${element.title}</h3>
+            <p id="color">Color: Black </p>
+            <p>Size:${element.size}</p>
+            <p>Style_No: 1556961</p>
+            <p>In Stock</p>
+            <label><a class = "remove" >Edit</a> | <a class = "remove removeBtn" data-id = ${ind}>Remove</a> </label>
+        </div>
+    
+        <div id="priceDiv">
+            <h3 id="price">${element.price}</h3>
+        </div>
+    
+        <div id="quantitys">
+            <select class="quantity" data-id = ${ind} style="margin-top: 20%;width: 60%;height: 15%;">
+                <option value="1">1</option>
+                <option value="2">2</option>
+                <option value="3">3</option>
+                <option value="4">4</option>
+                <option value="5">5</option>
+                <option value="6">6</option>
+                <option value="7">7</option>
+                <option value="8">8</option>
+                <option value="9">9</option>
+                <option value="10">10</option>
+    
+    
+            </select>
+        </div>
+    
+        <div id="subtotal">
+            <h3 id="subtotalValue">${element.price}</h3>
+        </div>
+    
+    </div>
+        `
+    });
+    appendCartData(htmlData);
+}
+
+function appendCartData(Data){
+    let mainContainer = `
+    <div id="main_container">
+    ${Data.join(" ")}
+    </div>`
+
+
+    container.innerHTML = mainContainer
+
+    valueAppend(cartData);
+    let removebtn = document.getElementsByClassName('removeBtn');
+    for(let k of removebtn){
+        k.addEventListener("click",(e)=>{
+            let i = e.target.dataset.id;
+            cartData.splice(i,1)
+            let newdata = cartData
+            localStorage.setItem("cartData",JSON.stringify(newdata));
+            RenderData(newdata);
+        })
+    }
+
+    let quantity = document.getElementsByClassName("quantity");
+
+    for(let k of quantity){
+        k.addEventListener("change",(e)=>{
+            let i = e.target.dataset.id;
+            cartData[i].count = k.value;
+            console.log(cartData[i])
+            localStorage.setItem("cartData",JSON.stringify(cartData));
+            RenderData(cartData);
+           
+        })
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// mai
---
+++ scripts/signup.js
@@ -0,0 +1,92 @@
+let S_btn=document.getElementById("signup_submit");
+let f_name=document.getElementById("F_Name");
+let l_name=document.getElementById("L_Name");
+let gender=document.getElementsByName("gender");
+  let phone=document.getElementById("Phone");
+
+let User_Data=JSON.parse(localStorage.getItem("login"))||[];
+let login_alert=document.getElementById("Login_password_alert")
+let baseurl="https://colambia-api.onrender.com";
+
+ S_btn.addEventListener("click",(e)=>{ 
+  e.preventDefault();
+  let g_value="";
+  for(i = 0; i < gender.length; i++) {
+    if(gender[i].checked){
+  
+            console.log("Gender: "+gender[i].value);
+            g_value=gender[i].value;
+    }
+  }   
+  console.log(g_value);
+  if(email.value!="" && pass.value!=""){
+         console.log("hii");
+              fetch(`${baseurl}/users`,{
+                  method:'POST',
+                  headers: {
+                      'Content-Type': 'application/json'
+                      
+                    },
+                    body: JSON.stringify({
+                        "email":email.value,
+                        "password":pass.value,
+                        "name":f_name.value+" "+l_name.value,
+                        "gender":g_value ,
+                        "Phone":phone.value
+                      })
+              }) .then((response) => response.json())
+              .then((data) => {
+                console.log('Success:', data);
+                getData();
+                document.getElementById("success").style.display="block";
+              })
+              .catch((error) => {
+                console.log('Error:', error);
+              });
+
+                  
+      }
+    }
+    )
+  
+      
+
+ 
+ async function getData() {
+   let res=await fetch(`${baseurl}/users`) 
+   let data=await res.json();
+   // console.log(data);
+    localStorage.setItem("login",JSON.stringify(data))
+ }
+
+ getData();
+ function login(){
+   let email=document.getElementById("L-email").value;
+   let pass=document.getElementById("L-password").value;
+   let is=false;
+   console.log(User_Data,email,pass);
+   User_Data.filter((el)=>{
+     if(el.email===email && el.password===pass){
+        is=true;
+      localStorage.setItem("user_login",JSON.stringify(el));
+
+     }
+     
+     
+   })
+   if(is){
+  document.getElementById("para").innerText=`Welcome ${email}`
+  document.getElementById("login_success").style.display="block"
+  document.getElementById("Login_password_alert").style.display="none";
+  
+   }
+   else
+    document.getElementById("Login_password_alert").style.display="flex"
+ }
+ email.addEventListener("focusout",(e)=>{
+      User_Data.filter((el)=>{
+        if(el.email===email.value){
+          document.getElementById(`${e.target.id}_warning`).innerText = `Already Login`;
+        }
+      })
+ })
